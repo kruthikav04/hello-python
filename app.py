1c3ac4f1e183fd1e64b2e1c31fe33ca0d024d7c9
Pooja <pp8927824@gmail.com>
2026-01-14 10:05:53 +0000

    Hello-python

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from prometheus_client import Counter, generate_latest
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_requests_total', 'Total app requests')
+
+@app.route('/')
+def hello():
+    REQUEST_COUNT.inc()
+    return "Hello DevOps World 🚀"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6001)
+
