10e35563e61e4ab7c0bf8bbbdbfe393694044c92
kruthikav04 <kruthika04v@gmail.com>
2026-01-16 15:46:29 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,7 @@
-output "namespace_name" {
-  description = "Created Kubernetes namespace"
-  value       = kubernetes_namespace_v1.dev.metadata[0].name
-}
-
 output "service_port" {
-  description = "NodePort exposed by the Python service"
-  value       = kubernetes_service_v1.python_service.spec[0].port[0].node_port
+  value = kubernetes_service_v1.python_service.spec[0].port[0].node_port
 }
 
+output "namespace_name" {
+  value = kubernetes_namespace_v1.dev.metadata[0].name
+}
