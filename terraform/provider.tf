fba7e6bfb68f57f2b589119716601ab9a78fedbd
kruthikav04 <kruthika04v@gmail.com>
2026-01-16 15:43:14 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,12 @@
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 3.0"
+      version = "~> 2.25"
     }
   }
 }
 
 provider "kubernetes" {
-  config_path = var.kubeconfig_path
+  config_path = "~/.kube/config"
 }
-
